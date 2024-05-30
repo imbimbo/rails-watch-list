86787c1033e9735f24d84b813558f60112fc4a27
felipe imbimbo <f_imbimbo@hotmail.com>
2024-05-30 15:56:39 -0300

    watch list almost done

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,36 @@
+class BookmarksController < ApplicationController
+  before_action :set_list, only: [:new, :create]
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    @bookmark.save
+    if @bookmark.save
+      # Data added
+      redirect_to list_path(@list)
+    else
+      # Error
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+end
